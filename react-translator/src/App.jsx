bc4c03a1f3ab143591999b41314bae2eb3ca01aa
Relativiteit <alejocain@gmail.com>
2025-05-01 21:25:33 +0200

    react app

--- react-translator/src/App.jsx
+++ react-translator/src/App.jsx
@@ -1,7 +1,21 @@
-import { useState, useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import './App.css'
 
+import LanguageSelector from './components/LanguageSelector';
+import Progress from './components/Progress';
 function App() {
+
+    // Model loading
+    const [ready, setReady] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+    const [progressItems, setProgressItems] = useState([]);
+  
+      // Inputs and outputs
+  const [input, setInput] = useState('I love walking my dog.');
+  const [sourceLanguage, setSourceLanguage] = useState('eng_Latn');
+  const [targetLanguage, setTargetLanguage] = useState('fra_Latn');
+  const [output, setOutput] = useState('');
+
    // Create a reference to the worker object 
    const worker = useRef(null);
 
@@ -22,12 +36,52 @@ function App() {
 
     // Define a cleanup function for when the component is unmounted.
     return () => worker.current.removeEventLIstener('message', onMessageReceived);
+
+    
   });
 
 
+  const translate = () => {
+    setDisabled(true);
+    setOutput('');
+    worker.current.postMessage({
+      text: input,
+      src_lang: sourceLanguage,
+      tgt_lang: targetLanguage,
+    });
+  }
+
   return (
     // TODO: rest of app here
-    console.log("")
+    <>
+    <h1>Transformers.js</h1>
+    <h2>ML-powered multilingual translation in React!</h2>
+
+    <div className='container'>
+      <div className='language-container'>
+        <LanguageSelector type={"Source"} defaultLanguage={"eng_Latn"} onChange={x => setSourceLanguage(x.target.value)} />
+        <LanguageSelector type={"Target"} defaultLanguage={"fra_Latn"} onChange={x => setTargetLanguage(x.target.value)} />
+      </div>
+
+      <div className='textbox-container'>
+        <textarea value={input} rows={3} onChange={e => setInput(e.target.value)}></textarea>
+        <textarea value={output} rows={3} readOnly></textarea>
+      </div>
+    </div>
+
+    <button disabled={disabled} onClick={translate}>Translate</button>
+
+    <div className='progress-bars-container'>
+      {ready === false && (
+        <label>Loading models... (only run once)</label>
+      )}
+      {progressItems.map(data => (
+        <div key={data.file}>
+          <Progress text={data.file} percentage={data.progress} />
+        </div>
+      ))}
+    </div>
+  </>
   ) 
 }
 
